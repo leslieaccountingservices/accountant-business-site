90299b0909894768ab07e1006f772375d5e690ed
rcabre95 <raphael.cabrera111@gmail.com>
2023-03-16 16:02:42 -0500

    completed testfor Header component

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import Link from "next/link";
 import Header from "../../components/Header";
 import '@testing-library/jest-dom'
 
@@ -8,5 +9,32 @@ describe('Header', () => {
 
         const head = screen.getByRole('banner');
         expect(head).toBeInTheDocument();
-    })
+    });
+
+    it('displays the main title', () => {
+        render(<Header page="home" />);
+
+        const title = screen.getByRole('heading');
+        expect(title).toBeInTheDocument();
+    });
+
+    const types: Array<string> = ["home", "blog", "post"];
+    const links = [
+      <Link href='/blog'><span className="">Blog</span></Link>,
+      <Link href='/faq'><span className="">FAQ</span></Link>,
+      null
+    ]
+    for (let i = 0; i < types.length; i++) {
+        it(`renders the appropriate link for ${types[i]}`, () => {
+            render(<Header page={types[i]} />)
+
+            const link = screen.getByTestId("headerLink");
+            expect(link).toBeInTheDocument();
+            if (types[i] === "home") {
+                expect(link).toHaveAttribute('href', "/blog");
+            } else {
+                expect(link).toHaveAttribute('href', '/faq')
+            }
+        })
+    };
 })
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { spawn } from "child_process"
 import Link from "next/link"
 import SocialLink from "./shared-ui/SocialLink"
 
-export default function Header({ page }: { page: "blog" | "home" | "post"}) {
+export default function Header({ page }: { page: string}) {
     return (
         <header className="fixed h-16 w-full px-8 flex justify-around items-center bg-red-300 z-10">
             <nav>
@@ -10,14 +10,12 @@ export default function Header({ page }: { page: "blog" | "home" | "post"}) {
             </nav>
             <h1 className="flex flex-1 justify-center text-center">Leslie's Accounting Services</h1>
             {page === "home" ? 
-            <Link href="/blog">
+            <Link data-testid="headerLink" href="/blog">
                 <span className="">Blog</span>
             </Link> : 
-            page === "blog" ? 
-            <Link href="/faq">
+            <Link data-testid="headerLink" href="/faq">
                 <span className="">FAQ</span>
-            </Link> : 
-            null}
+            </Link> }
         </header>
     )
 }
