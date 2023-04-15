2b913345798c48379c4e06721dfb4404daaa1d74
rcabre95 <raphael.cabrera111@gmail.com>
2023-04-15 14:10:48 -0500

    Header mobile friendly, cleaned up imports in home page

--- components/shared-ui/Header.tsx
+++ components/shared-ui/Header.tsx
@@ -1,48 +1,75 @@
 import Link from "next/link"
-import Image from "next/image";
-import SocialLink from "./SocialLink"
-import { useContext } from "react";
 import CalendarButton from "./CalendarButton";
 
 export default function Header({ isHome = false }: { isHome?: boolean }) {
     // put links under header as new div (for mobile/responsive)
     return (
-        <header className="fixed inset-0 h-16 w-full px-8 flex justify-around items-center bg-forest z-20">
-            <nav>
-                <Link className="" href={`/`}>
-                    <h1 className="text-xl font-extrabold text-white mr-2 italic">
-                        Leslie's Accounting Services
-                    </h1>
-                </Link>
-            </nav>
-            <nav className="h-4/6 flex flex-1 items-center border-l-2">
+        <header className="fixed flex flex-col inset-0 h-fit w-full   bg-forest z-20
+        ">
+            <div className="w-full h-16 flex justify-around items-center px-8">
+                <nav>
+                    <Link className="" href={`/`}>
+                        <h1 className="text-xl font-extrabold text-white mr-2 italic">
+                            Leslie's Accounting Services
+                        </h1>
+                    </Link>
+                </nav>
+                <nav className="h-4/6 sm:flex flex-1 items-center border-l-2 hidden">
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#about`}>
+                        About
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#services`}>
+                        Services
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#reviews`}>
+                        Reviews
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/blog`}>
+                        Blog
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/faq`}>
+                        FAQ
+                    </Link>
+                </nav>
+                <CalendarButton />
+            </div>
+            <div className="h-fit w-full bg-forest flex justify-around px-4 md:hidden">
                 <Link
-                className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
-                href={`/#about`}>
-                    About
-                </Link>
-                <Link
-                className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
-                href={`/#services`}>
-                    Services
-                </Link>
-                <Link
-                className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
-                href={`/#reviews`}>
-                    Reviews
-                </Link>
-                <Link
-                className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
-                href={`/blog`}>
-                    Blog
-                </Link>
-                <Link
-                className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
-                href={`/faq`}>
-                    FAQ
-                </Link>
-            </nav>
-            <CalendarButton />
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#about`}>
+                        About
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#services`}>
+                        Services
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/#reviews`}>
+                        Reviews
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/blog`}>
+                        Blog
+                    </Link>
+                    <Link
+                    className="text-sm text-white mx-2 underline hover:text-myorange cursor-pointer"
+                    href={`/faq`}>
+                        FAQ
+                    </Link>
+            </div>
         </header>
     )
 }
