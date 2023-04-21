80d7a0d37ab52bb952622b32b574213edb29f356
rcabre95 <raphael.cabrera111@gmail.com>
2023-04-21 13:24:13 -0500

    fixed calendar button issue

--- components/shared-ui/CalendarButton.tsx
+++ components/shared-ui/CalendarButton.tsx
@@ -19,11 +19,11 @@ export default function CalendarButton({ freeConsult }: { freeConsult: boolean }
 
   }, [])
 
-  return isWeb ? <button className="h-10 min-w-fit w-36 md:w-48 md:mb-2 mx-1 md:mx-0 border bg-navy px-1 text-bone rounded-md shadow-md md:transition md:ease-in-out md:delay-50 md:hover:scale-110" data-cal-link={`rcabrera1/${freeConsult ? 'rcabrera1/free-consultation' : 'rcabrera1'}`}>
+  return isWeb ? <button className="h-10 min-w-fit w-36 md:w-48 md:mb-2 mx-1 md:mx-0 border bg-navy px-1 text-bone rounded-md shadow-md md:transition md:ease-in-out md:delay-50 md:hover:scale-110" data-cal-link={`${process.env.NEXT_PUBLIC_CALCOM_USERNAME}${freeConsult ? '/free-consultation' : '/'}`}>
       { freeConsult ? "Free Consultation" : "Book now!" }
   </button> :
 
-  <Link target="_blank" href={`${freeConsult ? 'https://cal.com/rcabrera1/free-consultation' : "https://cal.com/rcabrera1/"}`}>
+  <Link target="_blank" href={`${freeConsult ? `https://cal.com/${process.env.NEXT_PUBLIC_CALCOM_USERNAME}/free-consultation` : `https://cal.com/${process.env.NEXT_PUBLIC_CALCOM_USERNAME}/`}`}>
     <button className="h-10 min-w-fit w-36 md:w-48 md:mb-2 mx-1 md:mx-0 border bg-navy px-1 text-bone rounded-md shadow-md md:transition md:ease-in-out md:delay-50 md:hover:scale-110">
       { freeConsult ? "Free Consultation" : "Book now!" }
     </button>
