a7d5be3956dffc621cefc0d922e665d43732105b
rcabre95 <raphael.cabrera111@gmail.com>
2023-04-21 19:43:01 -0500

    made faq responsive, fixed metatags httpEquiv

--- components/QandA.tsx
+++ components/QandA.tsx
@@ -3,8 +3,8 @@ export default function QandA({ question, answer }: { question: string, answer: 
 
     return (
         <details>
-            <summary data-testid="question" className="text-3xl mb-4">{question}</summary>
-            <p data-testid="answer" className="text-lg mb-4 ml-8">{answer}</p> 
+            <summary data-testid="question" className="text-2xl font-semibold md:font-normal md:text-3xl mb-4">{question}</summary>
+            <p data-testid="answer" className="text-lg mb-4 ml-4 md:ml-8">{answer}</p> 
         </details>
     )
 }
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -39,14 +39,14 @@ function Banner() {
     return (
     <div data-testid="faq-banner" className='w-full h-screen bg-bone'>
         <div className='w-full h-5/6 bg-gradient-to-r from-contrast to-forest flex justify-center'>
-            <section className=' h-4/6 w-4/6 flex items-center'>
-                <div className='h-3/6 w-4/6 flex flex-row'>
-                    <div className='h-full w-1/3 border-r border-black flex flex-col justify-center items-center'>
+            <section className=' h-4/6 w-full md:w-4/6 flex items-center'>
+                <div className='h-3/6 w-full md:w-4/6 flex flex-row'>
+                    <div className='hidden h-full w-1/3 border-r border-black md:flex flex-col justify-center items-center'>
                     <h4 className="text-8xl">FAQ</h4>
                     </div>
-                    <div className='h-full w-2/3 flex flex-col justify-center'>
-                    <h1 className='text-4xl font-light ml-4 my-4 text-white'>Frequently<br/>Asked<br/>Questions</h1>
-                    <p className='ml-4 my-4'>This is a list of the most common questions we get,<br/>and their answers.</p>
+                    <div className='h-full w-full md:w-2/3 flex flex-col justify-center'>
+                    <h1 className='text-4xl font-light ml-4 my-4 text-white'>Frequently <br className="hidden md:block"/>Asked <br className="hidden md:block"/>Questions</h1>
+                    <p className='text-base ml-4 my-4'>This is a list of the most common questions we get,<br/>and their answers.</p>
                     </div>
                 </div>
             </section>
@@ -58,8 +58,8 @@ function Banner() {
 function Main({ qas }: { qas: Array<FAQ>}) {
 
     return (
-        <div data-testid="faq-main" className='z-10 absolute w-full h-fit inset-y-1/2 flex flex-col justify-center items-center'>
-            <div className='w-4/6 h-fit min-h-80 transition ease-in-out duration-300 bg-bone border shadow-md rounded-md pl-8 pr-12 py-8 flex flex-col justify-between'>
+        <div data-testid="faq-main" className='z-10 absolute w-full h-fit inset-y-96 md:inset-y-1/2 flex flex-col justify-center items-center'>
+            <div className='w-full md:w-4/6 h-fit min-h-80 transition ease-in-out duration-300 bg-bone border shadow-md rounded-md pl-8 pr-12 py-8 flex flex-col justify-between'>
                 {qas.map(qa => (
                     <QandA key={qa.question} question={qa.question} answer={qa.answer} />
                     ))}
