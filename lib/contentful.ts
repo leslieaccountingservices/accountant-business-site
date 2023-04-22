1c1f248190ee88347b6eeb9fe09eb51d038e3f0b
rcabre95 <raphael.cabrera111@gmail.com>
2023-04-22 12:37:35 -0500

    moved api files and functions from api to contentful.ts

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -27,52 +27,107 @@ export interface FAQ {
 }
 
 export async function getPaths() {
-    var res;
+    const client = await contentful.createClient({
+        space: process.env.CONTENTFUL_SPACE_ID as string,
+        environment: 'master', // defaults to 'master' if not set
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string
+    });
 
-    try {
-        res = await fetch(`${process.env.NEXT_PUBLIC_HOME_URL}api/contentful/paths`);
-    } catch (err) {
-        throw new Error(`err: ${err}`)
-    }
+    const entries = await client.getEntries()
 
-    const paths = await res?.json();
+    let paths = entries.items.map(item => {
+        return {
+            params: { post: item.sys.id }
+        }
+    })
 
     return paths
 }
 
 export async function getPost(id: string) {
-    var res;
-    try {
-        res = await fetch(`${process.env.NEXT_PUBLIC_HOME_URL}api/contentful/one-post?id=${id}`);
+    const client = await contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string
+    });
 
-    } catch (err) {
-        throw new Error(`err: ${err}`)
+    const post = await client.getEntry(id as string);
+
+    const formatted: Entry = {
+        metadata: post.metadata,
+        id: post.sys.id,
+        createdAt: post.sys.createdAt,
+        updatedAt: post.sys.updatedAt,
+        type: post.sys.contentType.sys.id,
+        title: (post.fields as any).title,
+        description: (post.fields as any).description,
+        keywords: (post.fields as any).keywords,
+        headerImage: `http:${(post.fields as any).headerImage.fields.file.url}`,
+        body: (post.fields as any).body
     }
-    const post: Entry = await res?.json();
 
-    return post
+    return formatted
 }
 
 export async function getPosts(limit: number, skip: number) {
-    var res;
-    try {
-        res = await fetch(`${process.env.NEXT_PUBLIC_HOME_URL}api/contentful/all-posts?limit=${limit}&skip=${skip}`);
-    } catch(err) {
-        throw new Error(`err: ${err}`)
-    }
-    const posts: EntrySummary[] = await res?.json();
+    const client = await contentful.createClient({
+        space: process.env.CONTENTFUL_SPACE_ID as string,
+        environment: 'master', // defaults to 'master' if not set
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string
+    });
 
-    return posts
+    const entries = await client.getEntries({
+        content_type: 'main',
+        order: '-sys.createdAt',
+        limit: limit,
+        skip: skip,
+    })
+
+    // console.log(entries)
+
+    let formatted: EntrySummary[] = entries.items.map(item => (
+        {
+            metadata: item.metadata,
+            id: item.sys.id,
+            createdAt: item.sys.createdAt,
+            title: (item.fields as any).title,
+            thumbnail: `http:${(item.fields as any).thumbnail.fields.file.url}`
+        }
+    ))
+
+    return formatted
 }
 
 export async function getFaqs() {
+  const client = await contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string
+  });
+  
+  const faqs = await client.getEntries({
+    content_type: 'faq',
+    order: '-sys.createdAt'
+  })
+
+  const formatted: Array<FAQ> = faqs.items.map(item => (
+    {
+        question: (item.fields as any).question as string,
+        answer: (item.fields as any).answer as string
+    }
+  ))
+
+    return formatted
+}
+
+export async function morePosts(limit: number, skip: number) {
     var res;
     try {
-        res = await fetch(`${process.env.NEXT_PUBLIC_HOME_URL}api/contentful/faqs`);
+        res = await fetch(`${process.env.NEXT_PUBLIC_HOME_URL}api/contentful/all-posts?limit=${limit}&skip=${skip}`);
     } catch (err) {
-        throw new Error(`err: ${err}`)
+        throw new Error(`err: ${err}`);
     }
-    const faqs: FAQ[] = await res?.json();
 
-    return faqs
+    const posts: EntrySummary[] = await res.json();
+    return posts
 }
